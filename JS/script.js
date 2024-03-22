2b913a94ed50c6d94b2f75b7763dee4731ee4ede
Max Barnes <maxbarnes.dev@gmail.com>
2024-03-22 14:11:00 -0400

    more progress

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+/**
+ * create a new table row in the table
+ */
+
+function mapRow(food) {
+  const tr = document.createElement("tr");
+  const name = document.createElement("td");
+  const sugars = document.createElement("td");
+  const close = document.createElement("td");
+}
+
+/**
+ * search through sugar counter db
+ *
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const searchBox = document.querySelector(".search > input");
+
+  searchBox.addEventListener("keyup", (search) => {
+    // need to use query url here and show it as an option
+  });
+});
+
+/**
+ * add close to button on hover
+ */
